c5c7c9630c7436bf3904111f32e27dcf5d8fc72d
Renan Bezerra Gomes <renanbg90@gmail.com>
2022-05-06 20:07:21 -0300

    2.0
    Criando rotas crud

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
+
 app.get('/', (req,res) => {
-    // regra de negocio
-    res.json({ mensagem : 'Rota Funcionando'});
+    // RECUPERAR TODOS REGISTROS
+    res.json({ mensagem : 'RECUPERAR TODOS REGISTROS'});
+});
+
+app.get('/:id', (req,res) => {
+    // PEGAR SOMENTE O REGISTRO COM ID : 
+    const id = req.params.id;
+    res.json({ mensagem : `PEGAR SOMENTE O REGISTRO COM ID : ${id}` });
+});
+
+app.post('/', (req,res) => {
+    // CRIAR UM REGISTRO 
+    const body = req.body;
+    res.json({ body });
+});
+
+app.put('/:id', (req,res) => {
+    // ATUALIZAR O REGISTRO COM ID : 
+    const id = req.params.id;
+    res.json({ mensagem : `ATUALIZAR O REGISTRO COM ID : ${id}` });
+});
+
+app.delete('/:id', (req,res) => {
+    // DELETAR O REGISTRO COM ID : 
+    const id = req.params.id;
+    res.json({ mensagem : `DELETAR O REGISTRO COM ID :  ${id}` });
 });
 
 app.listen(3000, () => {
-    console.log('Meu servidor está funcionando')
+    console.log('Meu servidor está funcionando dd')
 })
