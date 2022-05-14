3f9c5c36c9d293f0f5b24255f732bb5d8b4c9a83
Renan Bezerra Gomes <renanbg90@gmail.com>
2022-05-13 21:47:02 -0300

    7.0
    Scripts de Importacao de usuarios, filmes, temporadas e episodeos

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const database = require('./src/services/dataBase');
 const app = express();
 
 const routes = require('./src/routes/filmes.routes');
-mongoose.connect('mongodb://localhost:27017/netflix',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
---
+++ src/scripts/addFilmes.js
@@ -0,0 +1,18 @@
+const database = require('../services/dataBase');
+
+const Filme = require('../models/filme');
+const filmesJSON = require('../data/filme.json')
+
+const addFilmes = async () => {
+    try {
+        for(let filme of filmesJSON){
+            console.log(`Inserindo ${filme.titulo}`);
+            await new Filme(filme).save();
+        }
+        console.log('Final do Script')
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+addFilmes();
